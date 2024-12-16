c213971ab73b0a81916c2c5d3d33e50a0a69f14a
iamrayghazali <151096230+iamrayghazali@users.noreply.github.com>
2024-12-16 23:09:18 +0100

    Done feature

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-const timeline = gsap.timeline({defaults : { duration : .25, opactiy : 0}})
-    timeline
-    .from("header a", { y: "-90px", stagger : .15})
-    .from("header:not(:last-child)", { y: "-90px", stagger : .15})
-    .from("header img", { y: "-500px", stagger : .15})
-    .from("#left-container p", {x : "-600px", stagger : .15})
-    .from("#right-container #ray1", {x : "1000px"})
-    .from("#right-container #ray2", {x : "1000px"})
-    .from("#right-container #ray3", {x : "1000px"})
-    gsap.to("header", { borderBottomColor: "white", duration: 3 });
 
