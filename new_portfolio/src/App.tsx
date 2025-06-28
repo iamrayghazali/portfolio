839268eff81071b9f7cd17315c08ae195b92c0f1
iamrayghazali <151096230+iamrayghazali@users.noreply.github.com>
2025-06-29 00:28:03 +0200

    Installed dependencies postcss autoprefixer gsap

--- new_portfolio/src/App.tsx
+++ new_portfolio/src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import ScrollBlurText from "./components/ScrollBlurText.tsx";
 
 function App() {
 
   return (
     <>
+      <div className="bg-black">
+    <ScrollBlurText></ScrollBlurText>
 
+      </div>
     </>
   )
 }
