9195d9c28a3bced726143d214d7da681dde41a09
iamrayghazali <151096230+iamrayghazali@users.noreply.github.com>
2025-08-04 10:26:18 +0200

    Done Hero feature

--- new_portfolio/src/App.tsx
+++ new_portfolio/src/App.tsx
@@ -1,20 +1,15 @@
-import React, {useEffect} from "react";
+import React from "react";
 import './App.css'
-// @ts-ignore
-import ScrollBlurText from "./components/ScrollBlurText.tsx";
 import {TracingBeam} from "./components/ui/tracing-beam";
-import {Menu, MenuItem, ProductItem, HoveredLink} from "./components/ui/navbar-menu";
 import {useGitHub} from "./context/GithubContext";
-import {TextHoverEffect} from "@/components/ui/text-hover-effect";
 import linkedInLogo from "./assets/logos/linkedin.svg"
 import gitHubLogo from "./assets/logos/github.svg"
 import emailLogo from "./assets/logos/email.svg"
-import {LampContainer, LampDemo} from "@/components/ui/lamp";
+import {LampDemo} from "@/components/ui/lamp";
+import {motion} from "framer-motion";
 
 
 function App() {
-    const [activeMenu, setActiveMenu] = React.useState<string | null>(null);
-    const [activeMenuItem, setActiveMenuItem] = React.useState<string | null>(null);
     const {repos, loading} = useGitHub();
 
     if (loading) {
@@ -27,58 +22,46 @@ function App() {
 
     return (
         <>
-            <div className="bg-black">
+            <TracingBeam>
 
-            <div className=" flex flex-col items-center p-5">
-                <Menu setActive={setActiveMenu}>
-                    {/*                <HoveredLink>
-                    <TextHoverEffect text={"RAY"} ></TextHoverEffect>
-                </HoveredLink>*/}
-                    <MenuItem setActive={setActiveMenuItem} active={activeMenuItem} item={"home"}></MenuItem>
+                <div className="bg-black">
+                    <div className="relative">
+                        <div className="absolute left-1/2 -translate-x-1/2 w-1/4 z-20 flex flex-col items-center p-5">
+                            <motion.div
+                                initial={{y: -100, opacity: 0}}
+                                animate={{y: 0, opacity: 1}}
+                                transition={{duration: 1.2, ease: "easeOut"}}
+                                className="pt-5 sticky flex items-center justify-between whitespace-nowrap text-white gap-4"
+                            >
+                                <a className=" transition-all duration-300 hover:text-cyan-500 active:scale-95 "
+                                   href="">
+                                    <p className="font-thin">home</p>
+                                </a>
+                                <a className="transition-colors duration-300 hover:text-cyan-500" href="">
+                                    <p className="font-thin">about</p>
+                                </a>
+                                <a className="transition-colors duration-300 hover:text-cyan-500" href="">
+                                    <p className="font-thin">projects</p>
+                                </a>
+                                <a className="">
+                                    <p className="font-bold text-transparent bg-clip-text drop-shadow-[0_0_4px_rgba(255,255,255,0.2)] bg-[linear-gradient(115deg,_rgba(255,255,255,0.05)_30%,_white_50%,_rgba(255,255,255,0.05)_70%)] bg-[length:300%_100%] bg-[position:200%_0] animate-[shine_6s_linear_infinite]">
+                                        let's talk
+                                    </p>
+                                </a>
+                            </motion.div>
+                        </div>
+                        <LampDemo text={"Hi, I'm Ray"}></LampDemo>
+                    </div>
 
-
-                    <MenuItem setActive={setActiveMenuItem} active={activeMenuItem} item={"projects"}>
-                        {
-                            repos && repos.map((repo) => (
-                                <div key={repo.id}>
-                                    <HoveredLink>
-                                        {/*TODO fix img loading from cache*/}
-                                        <ProductItem title={repo.name} description={repo.description}
-                                                     href={repo.screenshots[0].html_url}
-                                                     src={findMainScreenshot(repo.screenshots).download_url} img={true}>
-                                        </ProductItem>
-                                    </HoveredLink>
-                                </div>
-                            ))
-                        }
-                    </MenuItem>
-                    <MenuItem setActive={setActiveMenuItem} active={activeMenuItem} item={"let's talk"}>
-                        <ProductItem title={"LinedIn"} description={"Main way to connect, check out my profile!"}
-                                     href={"IMG"}
-                                     src={linkedInLogo} img={!!linkedInLogo}></ProductItem>
-                        <ProductItem title={"Github"} description={"You can see all my projects on GitHub."}
-                                     href={"IMG"}
-                                     src={gitHubLogo} img={!!gitHubLogo}></ProductItem>
-                        <ProductItem title={"Email"} description={"Email is the main way to connect with me."}
-                                     href={"IMG"}
-                                     src={emailLogo} img={!!emailLogo}></ProductItem>
-
-                    </MenuItem>
-
-
-                </Menu>
-            </div>
-            <TracingBeam className={""}>
-
-                    <LampDemo text={"Hi, I'm Ray"}></LampDemo>
-                    <div className="sticky left-0">
-
-                            <ScrollBlurText></ScrollBlurText>
+                    <div className={"h-screen bg-black"}></div>
+                    <div className={"h-screen bg-black"}>
 
                     </div>
+                    <div className={"h-screen bg-red-600"}>
+                    </div>
+                    <div className={"h-screen bg-green-600"}></div>
+                </div>
             </TracingBeam>
-            <div className={"h-screen bg-black"}></div>
-            </div>
 
         </>
     )
