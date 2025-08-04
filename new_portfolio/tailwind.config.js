cf3e1ddabb16a8b41abf5386619303340f4672b3
iamrayghazali <151096230+iamrayghazali@users.noreply.github.com>
2025-08-04 10:22:28 +0200

    Added shine animation to navbar link

--- new_portfolio/tailwind.config.js
+++ new_portfolio/tailwind.config.js
@@ -5,13 +5,12 @@ module.exports = {
         extend: {
             keyframes: {
                 shine: {
-                    '0%': { backgroundPosition: '0' },
-                    '60%': { backgroundPosition: '100%' },
-                    '100%': { backgroundPosition: '200px' },
+                    '0%': { backgroundPosition: '-200% 0' },
+                    '100%': { backgroundPosition: '200% 0' },
                 },
             },
             animation: {
-                shine: 'shine 3s linear infinite forwards',
+                shine: 'shine 2s linear infinite',
             },
         }
     },
