d7491843ef8c78afae65ce5828178573ee92c027
iamrayghazali <151096230+iamrayghazali@users.noreply.github.com>
2025-08-07 10:07:26 +0200

    Added timeline with dummy data

--- new_portfolio/src/App.tsx
+++ new_portfolio/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import './App.css'
 import {TracingBeam} from "./components/ui/tracing-beam";
 import {useGitHub} from "./context/GithubContext";
@@ -23,6 +23,10 @@ import {Timeline} from "@/components/ui/timeline";
 function App() {
     const {repos, loading} = useGitHub();
 
+    useEffect(() => {
+        console.log(repos);
+    }, []);
+
     if (loading) {
         return <h1>loading...</h1>
     }
@@ -30,6 +34,157 @@ function App() {
     const findMainScreenshot = (array) => {
         return array.find((screenshot) => screenshot.name === "main.png");
     }
+
+    const projectsArray = [
+        {
+            title: "JavaScript",
+            content: javaIMG + "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum",
+        }
+    ];
+
+    const data = [
+        {
+            title: "2024",
+            content: (
+                <div>
+                    <p className="mb-8 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
+                        Built and launched Aceternity UI and Aceternity UI Pro from scratch
+                    </p>
+                    <div className="grid grid-cols-2 gap-4">
+                        <img
+                            src="https://assets.aceternity.com/templates/startup-1.webp"
+                            alt="startup template"
+                            width={500}
+                            height={500}
+                            className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+                        />
+                        <img
+                            src="https://assets.aceternity.com/templates/startup-2.webp"
+                            alt="startup template"
+                            width={500}
+                            height={500}
+                            className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+                        />
+                        <img
+                            src="https://assets.aceternity.com/templates/startup-3.webp"
+                            alt="startup template"
+                            width={500}
+                            height={500}
+                            className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+                        />
+                        <img
+                            src="https://assets.aceternity.com/templates/startup-4.webp"
+                            alt="startup template"
+                            width={500}
+                            height={500}
+                            className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+                        />
+                    </div>
+                </div>
+            ),
+        },
+        {
+            title: "Early 2023",
+            content: (
+                <div>
+                    <p className="mb-8 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
+                        I usually run out of copy, but when I see content this big, I try to
+                        integrate lorem ipsum.
+                    </p>
+                    <p className="mb-8 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
+                        Lorem ipsum is for people who are too lazy to write copy. But we are
+                        not. Here are some more example of beautiful designs I built.
+                    </p>
+                    <div className="grid grid-cols-2 gap-4">
+                        <img
+                            src="https://assets.aceternity.com/pro/hero-sections.png"
+                            alt="hero template"
+                            width={500}
+                            height={500}
+                            className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+                        />
+                        <img
+                            src="https://assets.aceternity.com/features-section.png"
+                            alt="feature template"
+                            width={500}
+                            height={500}
+                            className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+                        />
+                        <img
+                            src="https://assets.aceternity.com/pro/bento-grids.png"
+                            alt="bento template"
+                            width={500}
+                            height={500}
+                            className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+                        />
+                        <img
+                            src="https://assets.aceternity.com/cards.png"
+                            alt="cards template"
+                            width={500}
+                            height={500}
+                            className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+                        />
+                    </div>
+                </div>
+            ),
+        },
+        {
+            title: "Changelog",
+            content: (
+                <div>
+                    <p className="mb-4 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
+                        Deployed 5 new components on Aceternity today
+                    </p>
+                    <div className="mb-8">
+                        <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
+                            ✅ Card grid component
+                        </div>
+                        <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
+                            ✅ Startup template Aceternity
+                        </div>
+                        <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
+                            ✅ Random file upload lol
+                        </div>
+                        <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
+                            ✅ Himesh Reshammiya Music CD
+                        </div>
+                        <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
+                            ✅ Salman Bhai Fan Club registrations open
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-2 gap-4">
+                        <img
+                            src="https://assets.aceternity.com/pro/hero-sections.png"
+                            alt="hero template"
+                            width={500}
+                            height={500}
+                            className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+                        />
+                        <img
+                            src="https://assets.aceternity.com/features-section.png"
+                            alt="feature template"
+                            width={500}
+                            height={500}
+                            className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+                        />
+                        <img
+                            src="https://assets.aceternity.com/pro/bento-grids.png"
+                            alt="bento template"
+                            width={500}
+                            height={500}
+                            className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+                        />
+                        <img
+                            src="https://assets.aceternity.com/cards.png"
+                            alt="cards template"
+                            width={500}
+                            height={500}
+                            className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+                        />
+                    </div>
+                </div>
+            ),}
+            ]
 
     return (
         <>
@@ -73,7 +228,7 @@ function App() {
                         </div>
                         <div className="m-10 mt-0 grid grid-cols-1 sm:grid-cols-2 gap-20">
                             <div className="sm:translate-y-25 flex flex-col text-center sm:text-start sm:justify-self-end gap-6 min-w-1/2">
-                                <h1 className="sm:text-3xl text-white text-lg font-bold font-fira">
+                                <h1 className="sm:text-3xl text-slate-200 text-lg font-bold font-fira">
                                     Programming Languages
                                 </h1>
                                 <div className="relative w-full max-w-[370px] aspect-[16/9] bg-black grid grid-cols-2 grid-rows-2 gap-8 place-items-center p-6">
@@ -89,7 +244,7 @@ function App() {
                             </div>
 
                             <div className="sm:-translate-y-25 sm:row-start-2 text-center sm:text-start sm:col-start-2 flex justify-center flex-col">
-                                <h1 className="sm:text-3xl sm:ml-20 text-white text-lg font-bold font-fira">
+                                <h1 className="sm:text-3xl sm:ml-20 text-slate-200 text-lg font-bold font-fira">
                                     Frameworks
                                 </h1>
 
@@ -114,7 +269,8 @@ function App() {
                         </div>
                     </section>
                 </TracingBeam>
-                <div className={"h-screen bg-red-600"}>
+                <div className={"h-screen bg-black"}>
+                    <Timeline data={data}></Timeline>
                 </div>
                 <div className={"h-screen bg-green-600"}></div>
             </div>
