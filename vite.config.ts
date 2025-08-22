7efd12b35a9e4455d6b1034e8753ab5a27931d9a
iamrayghazali <151096230+iamrayghazali@users.noreply.github.com>
2025-08-22 11:11:53 +0200

    Changed from absolute path to relative in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path"; // normal Node path
 
 export default defineConfig({
     plugins: [react()],
-    base: "/",
+    base: "./",
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "src"), // __dirname works in TS with Node
